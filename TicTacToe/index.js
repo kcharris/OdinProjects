5102423bf11cf0d4e122e9cef248cef1d9801648
kcharris <kcharris77@gmail.com>
2020-01-06 14:20:10 -0600

    add logic to return board status

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -2,6 +2,8 @@ const gameBoard = function(){
   
   let board = [1,2,3,4,5,6,7,8,9]
   let turn = 0
+  let player1Piece = 'X'
+  let player2Piece = 'O'
   const boardReset = function(){
     board = [1,2,3,4,5,6,7,8,9]
     turn = 0
@@ -18,8 +20,54 @@ const gameBoard = function(){
       }
     }
   }
-  const readBoard = () => board
-  return {playMove,boardReset,readBoard}
+  const readBoard = () => board;
+  const checkStatus = function() {  //returns 0 for nothing 1 for player1 2 for player2 and 3 for tie
+    let arr = readBoard
+    let p1 = player1Piece
+    let p2 = player2Piece
+    const drawChecker = function(arr){
+      if (/[1-9]/.test(arr.toString())){
+        return true
+      }
+      else false
+    }
+    const winChecker = function(p){
+      const verticalCheck = function(y){ //meant to take 0 1 2 which are indexes of bottom row
+        return (arr[y] + arr[y + 3] + arr[y + 6])
+      }
+      let end = false
+      switch (p + p + p){
+        case arr.slice(0,3):
+          end = true
+        case arr.slice(3,6):
+          end = true
+        case arr.slice(6,9):
+          end = true
+        case verticalCheck(0):
+          end = true
+        case verticalCheck(1):
+          end = true
+        case verticalCheck(2):
+          end = true
+        case (arr[0] + arr[4] + arr[8]):
+          end = true
+        case (arr[2] + arr[4] + arr[6]):
+          end = true
+      }
+      return end
+    }
+    if (winChecker(p1)){
+      return 1
+    }
+    else if (winChecker(p2)){
+      return 2
+    }
+    else if (drawChecker()){
+      return 3
+    }
+    else 0
+  }
+  return {playMove,boardReset,readBoard, checkStatus}
 }();
 const Player = function(str){
   let name = str
@@ -48,7 +96,7 @@ const displayController = function(){
       }    
     }(gameBoard)    
   }
-  createBoard = function(){
+  createBoard = function(){ //Buttons have logic that call renderGame
     div = document.getElementById("gameBoard")
     for(let i = 1; i < 10; i ++){
       let btn = document.createElement('input')
